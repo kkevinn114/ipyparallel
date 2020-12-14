567fdbc6fc478733e58be2cdae3a614a201d843d
Kevin <kkevinn114@gmail.com>
2020-12-14 11:01:33 -0800

    :art:

---
+++ .vscode/main.js
@@ -0,0 +1,5 @@
+/// <reference path="c:/Users/kkevi/.vscode/extensions/nur.script-0.0.6/global.d.ts" />
+// API - https://code.visualstudio.com/api/references/vscode-api 
+// reload window after edit...
+
+window.showInformationMessage('Hello World!')
